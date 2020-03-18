c2d26c5efc7dfe66c0d9b16d788a310d00abde6a
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-03-17 20:30:43 -0500

    add check comment ownership middleware

--- middleware/index.js
+++ middleware/index.js
@@ -27,4 +27,22 @@ middlewareObj.checkCollectionOwnership = function(req, res, next) {
   }
 };
 
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+      if (err || !comment) {
+        res.redirect('back');
+      } else {
+        if (comment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    res.redirect('back');
+  }
+};
+
 module.exports = middlewareObj;
